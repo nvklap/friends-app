1c7f233c2cd5357a7e4bf612ed5e681bd29c6a28
Nataliia Klapchuk <nataklap@gmail.com>
2022-09-26 13:07:08 +0300

    Rewrite sort and filter functions Refactor CSS

--- js/app.js
+++ js/app.js
@@ -1,25 +1,52 @@
-// Selectors
 const cardsWrapper = document.querySelector('.cards-wrapper');
 const form = document.querySelector('.form');
 
-// Consts
-const API = 'https://randomuser.me/api/';
-const USERS_QUANTITY = 30;
+const USERS_QUANTITY = 50;
 const USERS_COUNTRIES = 'gb,us,ua,fr,nl,nz';
+const API = 'https://randomuser.me/api/';
 const QUERY = `?results=${USERS_QUANTITY}&nat=${USERS_COUNTRIES}`;
 const API_URL = API + QUERY;
-// States and Arrs
 
 let usersData;
 let usersDataCopy;
 
-let friendsData;
-let tempArr;
-let filGen;
-let isSorted = false;
+getUsersData(API_URL)
+  .then((friends) => {
+    usersData = createUsersData(friends);
+    toggleDisabedOnForm(false);
+    renderUsersCards(usersData);
+  })
+  .catch((error) => {
+    alert('Something went wrong. Please reload the page!');
+    console.log(error);
+  });
 
-function handleUsersData(users) {
-  // create user arr of obj
+toggleDisabedOnForm(true);
+
+form.addEventListener('input', ({ target }) => {
+  if (target.tagName === 'INPUT') {
+    usersDataCopy = [...usersData];
+    uncheckPrevSortParameter(target.name);
+    const usersHandled = handleUsers(usersDataCopy, getFormData());
+    renderUsersCards(usersHandled);
+  }
+});
+
+form.addEventListener('reset', () => {
+  usersDataCopy = [...usersData];
+  renderUsersCards(usersDataCopy);
+});
+
+async function getUsersData(url) {
+  const response = await fetch(url);
+  if (!response.ok) {
+    throw new Error(`HTTP error! Status: ${response.status}`);
+  }
+  const { results } = await response.json();
+  return results;
+}
+
+function createUsersData(users) {
   users = users.map((user) => {
     const {
       name: { first: nameFirst, last: nameLast },
@@ -43,90 +70,18 @@ function handleUsersData(users) {
     };
   });
   return users;
-  // create cope
 }
 
-getUsersData(API_URL)
-  .then((friends) => {
-    usersData = handleUsersData([...friends]);
-    usersDataCopy = [...usersData];
-    renderUsersCards(usersData);
-
-    console.log(usersData);
-  })
-  .catch((error) => console.log(error));
-
-function getFormData() {
-  const formData = new FormData(form);
-  console.log(
-    formData.get('search'),
-    formData.get('name-order'),
-    formData.get('age'),
-    formData.get('gender')
-  );
-
-  const search = formData.get('search').trim();
-  const nameOrder = formData.get('name-order');
-  const ageOrder = formData.get('age');
-  const gender = formData.get('gender');
-  let sortBy;
-  if (nameOrder !== null) {
-    sortBy = nameOrder;
-  } else if (ageOrder !== null) {
-    sortBy = ageOrder;
-  }
-
-  return {
-    search,
-    sortBy,
-    gender,
-  };
-}
-
-function handleUsers(users, { search, sortBy, gender }) {
-  const filteredByName = filterUsersBySearch(users, search);
-  const filteredByGender = filterUsersByGender(filteredByName, gender);
-  const sortedUsers = sortUsers(filteredByGender, sortBy);
-  return sortedUsers;
-}
-
-form.addEventListener('change', ({ target }) => {
-  if (target.tagName === 'INPUT') {
-    usersDataCopy = [...usersData];
-    uncheckPrevSortParameter(target.name);
-    const usersHandled = handleUsers(usersDataCopy, getFormData());
-    cardsWrapper.innerHTML = '';
-    renderUsersCards(usersHandled);
-  }
-});
-
-function uncheckPrevSortParameter(currParameter) {
-  if (currParameter === 'age' || currParameter === 'name-order') {
-    // uncheckPrevSortParameter(target.name);
-    const prevParameter = currParameter === 'age' ? 'name-order' : 'age';
-    const inputs = document.querySelectorAll(`input[name='${prevParameter}']`);
-    inputs.forEach((input) => {
-      input.checked = false;
-    });
-  }
-}
-
-async function getUsersData(url) {
-  let response = await fetch(url);
-  if (!response.ok) {
-    throw new Error(`HTTP error! Status: ${response.status}`);
-  }
-  const { results } = await response.json();
-  return results;
+function toggleDisabedOnForm(boolean) {
+  Array.from(form.elements).forEach((element) => {
+    element.disabled = boolean;
+  });
 }
 
 function renderUsersCards(users) {
-  let usersCards = '';
-  users.forEach((user) => {
-    const userCard = createUserCard(user);
-    usersCards += userCard;
-  });
-  cardsWrapper.innerHTML += usersCards;
+  const usersCards = users.map((user) => createUserCard(user)).join('');
+  cardsWrapper.innerHTML = '';
+  cardsWrapper.innerHTML = usersCards;
 }
 
 function createUserCard(user) {
@@ -152,92 +107,106 @@ function createUserCard(user) {
         <img class="card-img" src="${userPic}" alt="${nameFirst} photo" />
         <div class="card-field">
           <span class="card-field-name">Name: </span>
-          <span>${nameFirst} ${nameLast}</span>
+          <span class="card-field-value">${nameFirst} ${nameLast}</span>
         </div>
         <div class="card-field">
           <span class="card-field-name">From: </span>
-          <span>${city}, ${country}</span>
+          <span class="card-field-value">${city}, ${country}</span>
         </div>
         <div class="card-field">
           <span class="card-field-name">Email: </span>
-          <span>${email}</span>
+          <span class="card-field-value">${email}</span>
         </div>
         <div class="card-field">
           <span class="card-field-name">Tel: </span>
-          <span>${phone}</span>
+          <span class="card-field-value">${phone}</span>
         </div>
       </div>
       <div class="card-contact ${gender}">
         <a class="contact ${gender}" href="tel:+${phone}"> call me </a>
-        <a class="contact ${gender}" href="tel:+${phone}"> text me </a>
+        <a class="contact ${gender}" href="mailto:${email}"> text me </a>
       </div>
     </li>
   `;
 }
 
-function sortUsersByName(users, sortType) {
-  if (sortType === 'ascending') {
-    users.sort((currentUser, nextUser) => {
-      return currentUser.nameFirst > nextUser.nameFirst ? 1 : -1;
-    });
-  }
-  if (sortType === 'descending') {
-    users.sort((currentUser, nextUser) => {
-      return currentUser.nameFirst > nextUser.nameFirst ? -1 : 1;
+function uncheckPrevSortParameter(currParameter) {
+  if (currParameter === 'age' || currParameter === 'name-order') {
+    const prevParameter = currParameter === 'age' ? 'name-order' : 'age';
+    const inputs = document.querySelectorAll(`input[name='${prevParameter}']`);
+    inputs.forEach((input) => {
+      input.checked = false;
     });
   }
 }
 
-function sortUsersByAge(users, sortType) {
-  if (sortType === 'younger') {
-    users.sort((currentUser, nextUser) => {
-      return currentUser.age - nextUser.age;
-    });
-  }
-  if (sortType === 'older') {
-    users.sort((currentUser, nextUser) => {
-      return nextUser.age - currentUser.age;
-    });
-  }
+function getFormData() {
+  const formData = new FormData(form);
+  const search = formData
+    .get('search')
+    .replace(/\s+/g, ' ')
+    .trim()
+    .toLowerCase();
+  const gender = formData.get('gender');
+  const sortBy = getSortType(formData);
+
+  return {
+    search,
+    sortBy,
+    gender,
+  };
+}
+
+function getSortType(formData) {
+  const nameOrder = formData.get('name-order');
+  const ageOrder = formData.get('age');
+  if (nameOrder === null && ageOrder === null) return undefined;
+  return nameOrder !== null ? nameOrder : ageOrder;
+}
+
+function handleUsers(users, { search, sortBy, gender }) {
+  const filteredByName = filterUsersBySearch(users, search);
+  const filteredByGender = filterUsersByGender(filteredByName, gender);
+  const sortedUsers = sortUsers(filteredByGender, sortBy);
+  return sortedUsers;
+}
+
+function filterUsersBySearch(users, search) {
+  if (search === '') return users;
+  return users.filter(({ nameFirst, nameLast }) => {
+    const name = `${nameFirst} ${nameLast}`.toLowerCase();
+    return name.includes(search);
+  });
+}
+
+function filterUsersByGender(users, genderFilter) {
+  if (genderFilter === 'all') return users;
+  return users.filter(({ gender }) => gender === genderFilter);
 }
 
 function sortUsers(users, sortType) {
   switch (sortType) {
     case undefined:
       return users;
-      break;
     case 'ascending':
-      return users.sort((currentUser, nextUser) => {
-        return currentUser.nameFirst > nextUser.nameFirst ? 1 : -1;
-      });
-      break;
+      return users.sort(compareNames);
     case 'descending':
-      return users.sort((currentUser, nextUser) => {
-        return currentUser.nameFirst > nextUser.nameFirst ? -1 : 1;
-      });
-      break;
+      return users.sort((currentUser, nextUser) =>
+        compareNames(nextUser, currentUser)
+      );
     case 'younger':
-      return users.sort((currentUser, nextUser) => {
-        return currentUser.age - nextUser.age;
-      });
+      return users.sort(compareAges);
     case 'older':
-      return users.sort((currentUser, nextUser) => {
-        return nextUser.age - currentUser.age;
-      });
+      return users.sort((currentUser, nextUser) =>
+        compareAges(nextUser, currentUser)
+      );
   }
 }
 
-function filterUsersByGender(users, genderFilter) {
-  users = [...usersData];
-  if (genderFilter === 'all') return users;
-  return users.filter(({ gender }) => {
-    // console.log(userGender === genderFilter);
-    return gender === genderFilter;
-  });
+function compareNames(firstUser, secondUser) {
+  return firstUser.nameFirst > secondUser.nameFirst ? 1 : -1;
 }
 
-function filterUsersBySearch(users, search) {
-  if (search === '') {
-    return users;
-  }
+function compareAges(firstUser, secondUser) {
+  return firstUser.age - secondUser.age;
 }
